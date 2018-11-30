764195ed00e3ecfeb0cc3821655dc9a49ed28de0
Francisco Javier Hidalgo Frias <friasjavi45@gmail.com>
2018-11-30 12:11:34 +0100

    MaterialSesign08 - Finalizado

--- app/src/main/java/com/iesvirgendelcarmen/dam/materialdesign08/Material08.java
+++ app/src/main/java/com/iesvirgendelcarmen/dam/materialdesign08/Material08.java
@@ -8,6 +8,6 @@ public class Material08 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.app1);
+        setContentView(R.layout.app2);
     }
 }
